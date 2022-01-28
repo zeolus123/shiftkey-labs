357c769705a3b2c0304daea39c3088ccd3c69b53
Zachery Spencer <zc321357@dal.ca>
2022-01-27 23:20:56 -0400

    Update tweet_profiler.py

--- week5/profiler/profileGrabber/tweet_profiler.py
+++ week5/profiler/profileGrabber/tweet_profiler.py
@@ -2,11 +2,6 @@ import tweepy
 import time
 from collections import defaultdict
 
-#twitter API keys
-consumer_key = "qkAJRE0PK4hQOfJbfB7IAIvDI"
-consumer_secret = "1nLFjCAT3OjW6aHOYHZq2GeXkn4lvdKs4GGwo4TleZ3U4gEc97"
-access_key = "103593331-eDe1tVpxandq4ae6VHS38MXrzDisq2Jgt36DyLpl"
-access_secret = "i4Q8fdHDMIIHS6asGj1w8ouKPfonbMV11LiQQumPTTHMW"
 
 def get_profile(screen_name):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
